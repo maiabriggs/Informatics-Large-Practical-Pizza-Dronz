53217219d47ecbe8ca9a22d0df76b306186fb8b2
Maia <maia@briggs.nz>
2023-10-01 13:51:05 +0100

    Added order code to OrderValidator and gained a clearer idea of how it's working.

---
+++ src/main/java/uk/ac/ed/inf/LngLatHandler.java
@@ -0,0 +1,29 @@
+package uk.ac.ed.inf;
+
+import uk.ac.ed.inf.ilp.data.LngLat;
+import uk.ac.ed.inf.ilp.data.NamedRegion;
+import uk.ac.ed.inf.ilp.data.Order;
+import uk.ac.ed.inf.ilp.data.Restaurant;
+import uk.ac.ed.inf.ilp.interfaces.LngLatHandling;
+public class LngLatHandler implements LngLatHandling {
+
+    @Override
+    public double distanceTo(LngLat startPosition, LngLat endPosition) {
+        return 0;
+    }
+
+    @Override
+    public boolean isCloseTo(LngLat startPosition, LngLat otherPosition) {
+        return false;
+    }
+
+    @Override
+    public boolean isInRegion(LngLat position, NamedRegion region) {
+        return false;
+    }
+
+    @Override
+    public LngLat nextPosition(LngLat startPosition, double angle) {
+        return null;
+    }
+}
--- src/main/java/uk/ac/ed/inf/OrderValidator.java
+++ src/main/java/uk/ac/ed/inf/OrderValidator.java
@@ -3,6 +3,7 @@ package uk.ac.ed.inf;
 import uk.ac.ed.inf.ilp.data.Order;
 import uk.ac.ed.inf.ilp.data.Restaurant;
 import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
+import uk.ac.ed.inf.ilp.constant.*;
 
 /**
  * a sample order validator which does nothing
@@ -10,6 +11,7 @@ import uk.ac.ed.inf.ilp.interfaces.OrderValidation;
 public class OrderValidator implements OrderValidation {
     @Override
     public Order validateOrder(Order orderToValidate, Restaurant[] definedRestaurants) {
-        return null;
+        orderToValidate.setOrderValidationCode(OrderValidationCode.NO_ERROR);
+        return orderToValidate;
     }
 }
