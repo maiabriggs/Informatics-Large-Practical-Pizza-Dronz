2e2b08eb8846cb37bea49b14742355023d46f5be
Maia <maia@briggs.nz>
2023-11-28 20:58:44 +0000

    Added comments and its not working anymore

--- src/main/java/uk/ac/ed/inf/NewFlightPathCalculator.java
+++ src/main/java/uk/ac/ed/inf/NewFlightPathCalculator.java
@@ -32,38 +32,48 @@ public class NewFlightPathCalculator {
         while (!openSet.isEmpty()){
             Move currMove = openSet.poll();
             LngLat curr = currMove.getCurrLngLat();
+            System.out.println("This is out current lowest cost move");
+            System.out.println(curr.lng());
+            System.out.println(curr.lat());
+            System.out.println("Total: " + currMove.getTotal());
+
             count += 1;
             for (Double direction : Direction.getAllDirections()) {
+                System.out.println("Direction: " + direction);
                 LngLat neighbour = lngLatHandler.nextPosition(curr, direction);
+                System.out.println("Neighbour position lng: " + neighbour.lng() + " lat: " + neighbour.lat());
                 Move neighbourMove = new Move(orderNo, neighbour);
                 //Check if neighbour is the destination
                 if (lngLatHandler.isCloseTo(end, neighbour)) {
+                    System.out.println("We've reached the end");
                     //Do all the things that should be done if we've found the path
                     neighbourMove.setParent(currMove);
                     return tracePath(neighbourMove);
                 }
 
+                //Check that there is not a better path option
                 if (!isOnClosedSet(neighbourMove) && !inNoFly(noFly, neighbour) && !lngLatHandler.isInRegion(neighbour, centralArea) || count == 999999999) {
-                    //Find g and h
-                    neighbourMove.setG(currMove.getG() + 10);
-                    neighbourMove.setH(lngLatHandler.distanceTo(end, neighbour));
-
-                    //Calculate total
-                    neighbourMove.setTotal(neighbourMove.getG() + neighbourMove.getH());
-
+                    System.out.println("Not on closed set or FORBIDDEN");
                     if (isLowerOnOpenSet(neighbourMove)) {
+                        System.out.println("There's a better move");
                         neighbourMove.setParent(betterMove);
-                        neighbourMove.setG(betterMove.getG() + 10);
+                        System.out.println("Better Move lng: " + betterMove.getCurrLngLat().lng() + " lat: " + betterMove.getCurrLngLat().lat());
+                        neighbourMove.setG(betterMove.getG() + 1);
+                        neighbourMove.setH(lngLatHandler.distanceTo(end, neighbour));
                         neighbourMove.setTotal(neighbourMove.getG() + neighbourMove.getH());
                     }
                     else {
+                        System.out.println("Theres no better move, we are adding this neighbour to the open set");
                         neighbourMove.setParent(currMove);
+                        neighbourMove.setG(currMove.getG() + 1);
+                        neighbourMove.setH(lngLatHandler.distanceTo(end, neighbour));
+                        neighbourMove.setTotal(neighbourMove.getG() + neighbourMove.getH());
                         openSet.add(neighbourMove);
-
                     }
                 }
             }
             closedSet.add(currMove);
+            System.out.println("Current gets marked as visited and we repeat !!!!!!!!!!!");
         }
 
         throw new RuntimeException("Cannot find a path");
